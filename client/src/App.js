c48a5adcc74d14f4d9e27ada03098f3f39441f6c
Vincent McGargill <vincentmcgargill@gmail.com>
2020-11-09 15:42:40 -0700

    Updating it so that the nav is inside the browser router. Hopefully this will help the app deploy on heroku correctly.

--- client/src/App.js
+++ client/src/App.js
@@ -26,71 +26,71 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Nav />
       <div class="container mainContainer">
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" >
-              <Home/>
-            </Route>
-            <Route exact path="/signup" >
-              <Signup/>
-            </Route>
-            <Route exact path="/login" >
-              <Login/>
-            </Route>
-            <Route exact path="/postProduct" >
-              <PostNewProduct/>
-            </Route>
-            <Route exact path="/product/:id" >
-              <Product/>
-            </Route>
-            <Route exact path="/editProduct/:id" >
-              <EditProduct/>
-            </Route>
-            <Route exact path="/deleteProduct/:id" >
-              <DeleteProduct/>
-            </Route>
-            <Route exact path="/merchant/:id" >
-              <Merchant/>
-            </Route>
-            <Route exact path="/accountSettings" >
-              <AccountSettings/>
-            </Route>
-            <Route exact path="/editProfile" >
-              <EditProfile/>
-            </Route>
-            <Route exact path="/updateName" >
-              <UpdateName/>
-            </Route>
-            <Route exact path="/updateEmail" >
-              <UpdateEmail/>
-            </Route>
-            <Route exact path="/updatePassword" >
-              <UpdatePassword/>
-            </Route>
-            <Route exact path="/sellingAccount" >
-              <SellingAccount/>
-            </Route>
-            <Route exact path="/wishList" >
-              <WishList/>
-            </Route>
-            <Route exact path="/userCart" >
-              <Cart/>
-            </Route>
-            <Route exact path="/cartAdded/:id" >
-              <CartAdded/>
-            </Route>
-            <Route exact path="/searchResults/:search" >
-              <SearchResualts/>
-            </Route>
-            <Route>
+        <Switch>
+          <Route exact path="/" >
+            <Home/>
+          </Route>
+          <Route exact path="/signup" >
+            <Signup/>
+          </Route>
+          <Route exact path="/login" >
+            <Login/>
+          </Route>
+          <Route exact path="/postProduct" >
+            <PostNewProduct/>
+          </Route>
+          <Route exact path="/product/:id" >
+            <Product/>
+          </Route>
+          <Route exact path="/editProduct/:id" >
+            <EditProduct/>
+          </Route>
+          <Route exact path="/deleteProduct/:id" >
+            <DeleteProduct/>
+          </Route>
+          <Route exact path="/merchant/:id" >
+            <Merchant/>
+          </Route>
+          <Route exact path="/accountSettings" >
+            <AccountSettings/>
+          </Route>
+          <Route exact path="/editProfile" >
+            <EditProfile/>
+          </Route>
+          <Route exact path="/updateName" >
+            <UpdateName/>
+          </Route>
+          <Route exact path="/updateEmail" >
+            <UpdateEmail/>
+          </Route>
+          <Route exact path="/updatePassword" >
+            <UpdatePassword/>
+          </Route>
+          <Route exact path="/sellingAccount" >
+            <SellingAccount/>
+          </Route>
+          <Route exact path="/wishList" >
+            <WishList/>
+          </Route>
+          <Route exact path="/userCart" >
+            <Cart/>
+          </Route>
+          <Route exact path="/cartAdded/:id" >
+            <CartAdded/>
+          </Route>
+          <Route exact path="/searchResults/:search" >
+            <SearchResualts/>
+          </Route>
+          <Route>
             <NoMatch />
           </Route>
-          </Switch>
-        </BrowserRouter>
+        </Switch>
       </div>
       <Footer />
+      </BrowserRouter>
     </div>
   );
 }
