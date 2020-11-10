e442de79faee2775055d5e7b28dc54fd1c412f5a
Vincent McGargill <vincentmcgargill@gmail.com>
2020-11-09 17:22:36 -0700

    Adding get * route to app.js

--- app.js
+++ app.js
@@ -23,6 +23,10 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 require("./routes/api-routes.js")(app);
 require("./routes/product-api-routes.js")(app);
 require("./routes/user-api-routes.js")(app);
