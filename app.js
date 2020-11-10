be702f04863841f8da83ff1591d2e650463e45c0
Vincent McGargill <vincentmcgargill@gmail.com>
2020-11-09 20:45:26 -0700

    Changing * to / to see if that fixes the login problem

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(passport.session());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(__dirname + "/client/build"));
 
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "/client/build/index.html"));
   });
 }
