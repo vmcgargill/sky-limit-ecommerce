ca1d6d87ae7adceb925cad6cec22a77132ddf71e
Vincent McGargill <vincentmcgargill@gmail.com>
2020-11-09 18:23:47 -0700

    Trying something else to get Heroku deployment to work. Removed the path.join from line 25

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname + "client/build")));
+  app.use(express.static(__dirname + "client/build"));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
   });
