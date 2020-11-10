e1600055a71589ad9477e71c442ed6654cbeef96
Vincent McGargill <vincentmcgargill@gmail.com>
2020-11-09 18:15:33 -0700

    Moving the app into the if production statement

--- app.js
+++ app.js
@@ -20,12 +20,11 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname + "client/build")));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  });
 }
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
 
 require("./routes/api-routes.js")(app);
 require("./routes/product-api-routes.js")(app);
