48340b7cf3d986ddf5cbc2cd861f4113ddd6e757
Vincent McGargill <vincentmcgargill@gmail.com>
2020-11-09 20:37:56 -0700

    Trying to stringify object fo fix problem just the response

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -55,7 +55,7 @@ function Nav() {
   useEffect(() => {
     console.log("------------------------------------")
     API.getUserLoginStatus().then((res) => {
-      console.log("====================================" + JSON.stringify(res.data.message))
+      console.log("====================================" + JSON.stringify(res))
       if (res.data.message) {
         setNavOptions(LoggedInNav);
       } else {
