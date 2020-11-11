1c50407d804a5c6cc425171f3e511fbfe796b376
Vincent McGargill <vincentmcgargill@gmail.com>
2020-11-11 12:18:45 -0700

    Fixed cart.js so that it will calculate and display the cart total for the user to see.

--- client/src/pages/User/Cart.js
+++ client/src/pages/User/Cart.js
@@ -4,70 +4,74 @@ import ConvertImage from '../../ConvertImage'
 import './Cart.css';
 
 const Cart = () => {
-  const [cart, setCart] = useState([]);
-  const [order, setOrder] = useState("");
-
-  const removeCart = (id) => {
-    API.removeCart(id).then(function() {
-      LoadCart();
-    })
-  }
-
-  const placeOrder = (orderCart) => {
-    console.log("Order has been placed: " + orderCart);
-  }
-
-  const LoadCart = () => {
-    API.loadCart().then(res => {
-      if (res.data.products) {
-        if (res.data.products.length === 0) {
-          setCart([]);
-          setOrder(<h5>Your cart is currently empty.</h5>);
-        } else {
-          setCart(res.data.products);
-          setOrder(<button className="btn btn-primary merchantBtn" onClick={() => {placeOrder(cart)}}>Place Order</button>);
-        }
-      }
-    });
-  }
+  const [cart, setCart] = useState("");
+  const [orderBtn, setOrderBtn] = useState("");
+  const [cartTotal, setCartTotal] = useState("");
   
-  const cartItems = cart.map((product) => {
-    let productImg = "";
-
-    if (product.image === undefined) {
-      productImg = "/Default.jpg";
-    } else {
-      productImg = "data:image/jpeg;base64," + ConvertImage(product.image.data.data);
+  useEffect(() => {
+    const LoadCart = () => {
+      API.loadCart().then(res => {
+        if (res.data.products) {
+          if (res.data.products.length === 0) {
+            setCart("");
+            setOrderBtn(<h5>Your cart is currently empty.</h5>);
+          } else {
+            const currentCartTotal = res.data.products.map(product => product.price).reduce((x, y) => x + y, 0);
+            setCartTotal(currentCartTotal.toString());
+  
+            setOrderBtn(<button className="btn btn-primary merchantBtn" onClick={() => {
+              console.log("Order has been placed. The price set for comparison is: " + currentCartTotal);
+            }}>Place Order</button>);
+  
+            const cartList = res.data.products.map((product) => {
+              let productImg = "";
+              if (product.image === undefined) {
+                productImg = "/Default.jpg";
+              } else {
+                productImg = "data:image/jpeg;base64," + ConvertImage(product.image.data.data);
+              }
+          
+              return (
+                <div className="card mb-3" key={product._id}>
+                  <div className="row no-gutters">
+                    <div className="col-md-4">
+                    <div className="imageDiv"><img src={productImg} className="card-img cartListImg" alt="..."></img></div>
+                    </div>
+                    <div className="col-md-4">
+                      <div className="card-body">
+                        <h5 className="card-title">{product.name}</h5>
+                        <button className="btn btn-danger merchantBtn" onClick={() => {
+                          API.removeCart(product._id).then(() => {
+                            LoadCart();
+                          })
+                        }}>Remove from Cart</button>
+                      </div>
+                    </div>
+                    <div className="col-md-4">
+                    <div className="card-body">
+                        <h5 className="card-title">${product.price}</h5>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )
+            });
+            setCart(cartList);
+          }
+        }
+      });
     }
 
-    return (
-      <div className="card mb-3" key={product._id}>
-        <div className="row no-gutters">
-          <div className="col-md-4">
-          <div className="imageDiv"><img src={productImg} className="card-img cartListImg" alt="..."></img></div>
-          </div>
-          <div className="col-md-8">
-            <div className="card-body">
-              <h5 className="card-title">{product.name}</h5>
-              <p className="card-text">${product.price}</p>
-              <p className="card-text merchantDescription">{product.description}</p>
-              <p className="card-text"><small className="text-muted">Added to cart 10 days ago.</small></p>
-              <button className="btn btn-danger merchantBtn" onClick={() => {removeCart(product._id)}}>Remove from Cart</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  });
+    LoadCart();
 
-  useEffect(LoadCart, [cart])
+  }, [])
 
   return (
     <div className="container">
       <h2>Cart</h2>
-      {cartItems}
-      <h5>Cart Total: $100</h5>
-      {order}
+      {cart}
+      <h5>Cart Total: ${cartTotal}</h5>
+      {orderBtn}
     </div>
   )
 
