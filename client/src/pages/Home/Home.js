da0517174e547f67431af34fbcdb0f548720ecb9
Vincent McGargill <vincentmcgargill@gmail.com>
2020-11-11 15:01:43 -0700

    Added the loading icon to the home page. Now when the home page is still loading products it will display the loading icon until the products are rendered. This icon is good for when we are loading pages with long lists of products, but for pages with one product it's best to leave it as is because those pages load faster. Also should add a mini loading icon for logins and signups.

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from "react";
 import ProductList from "../../components/Product/ProductList"
 import API from "../../utils/API";
+import LoadingIcon from "../../components/LoadingIcon/LoadingIcon"
 import "./Home.css"
 
 function Home() {
   const [products, setProducts] = useState ([]);
+  const [load, setLoad] = useState(LoadingIcon);
 
   useEffect(() => {
     API.getProducts("/api/products").then(res => {
       if (res.data) {
+        setLoad("")
         setProducts(res.data);
       }
     });
@@ -17,6 +20,7 @@ function Home() {
   return (
     <div className="home">
       <h2>Shop for the Holidays</h2>
+      {load}
       <div className="row">
         <ProductList products={products}/>
       </div>
