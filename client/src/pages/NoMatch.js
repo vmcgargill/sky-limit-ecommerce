441ae5a490be9747b757ac254d5d60ad75f6346f
Vincent McGargill <vincentmcgargill@gmail.com>
2020-11-22 22:04:18 -0700

    Added an extra message to the 404 page saying that if they ran into that page trying to find a product, that product may no longer exist.

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -4,6 +4,7 @@ function NoMatch() {
   return (
     <div>
       <h1>404 Error: Page does not exist.</h1>
+      <h5>If it is a product or selling account that you are trying to reach, this item could have been removed and may no longger exist.</h5>
     </div>
   );
 }
