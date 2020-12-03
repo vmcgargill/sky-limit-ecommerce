757a332094060f1fddaae9ec264591b128e8f98e
Vincent McGargill <vincentmcgargill@gmail.com>
2020-12-03 11:46:14 -0700

    Added the checkout form components to separate it from the Checkout.js page.

---
+++ client/src/components/Order/CheckoutForm.js
@@ -0,0 +1,39 @@
+import {CardElement, useStripe, useElements} from '@stripe/react-stripe-js';
+// import API from "../../utils/API";
+
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: elements.getElement(CardElement)
+    })
+
+    if (!error) {
+      console.log(paymentMethod)
+    }
+
+    // API.placeOrder(currentCartTotal).then(res => {
+    //   if (!res.data.orderStatus) {
+    //     setError(<Error message={res.data.message}/>)
+    //     LoadCart()
+    //   } else if (res.data.orderStatus) {
+    //     window.location.href = "/confirmOrder/" + res.data.id;
+    //   }
+    // })
+
+  }
+
+  return (
+    <form style={{maxWidth: "400px", margin: "0 auto"}} onSubmit={handleSubmit}>
+      <CardElement/><br/>
+      <button type="submit" className="btn btn-success">Submit order</button>
+    </form>
+  )
+}
+
+export default CheckoutForm;
--- client/src/pages/Order/Checkout.js
+++ client/src/pages/Order/Checkout.js
@@ -1,43 +1,11 @@
 import React from "react"
 // import API from "../../utils/API";
-import {Elements, CardElement, useStripe, useElements} from '@stripe/react-stripe-js';
+import {Elements} from '@stripe/react-stripe-js';
+import CheckoutForm from "../../components/Order/CheckoutForm"
+import "./Checkout.css"
 import {loadStripe} from '@stripe/stripe-js';
+
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
-
-const CheckoutForm = () => {
-  const stripe = useStripe();
-  const elements = useElements();
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    const { error, paymentMethod } = await stripe.createPaymentMethod({
-      type: "card",
-      card: elements.getElement(CardElement)
-    })
-
-    if (!error) {
-      console.log(paymentMethod)
-    }
-
-    // API.placeOrder(currentCartTotal).then(res => {
-    //   if (!res.data.orderStatus) {
-    //     setError(<Error message={res.data.message}/>)
-    //     LoadCart()
-    //   } else if (res.data.orderStatus) {
-    //     window.location.href = "/confirmOrder/" + res.data.id;
-    //   }
-    // })
-
-  }
-
-  return (
-    <form style={{maxWidth: "400px", margin: "0 auto"}} onSubmit={handleSubmit}>
-      <CardElement/>
-      <button type="submit">Pay</button>
-    </form>
-  )
-}
 
 const Checkout = () => {
 
@@ -45,7 +13,6 @@ const Checkout = () => {
     <div>
       <h2>Checkout</h2>
       <Elements stripe={stripePromise}>
-        <h1>Hello</h1>
         <CheckoutForm/>
       </Elements>
     </div>
